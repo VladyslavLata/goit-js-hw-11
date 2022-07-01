489f60eba979c8e27e37e49e66b3197104c16c65
VladyslavLata <vladlatahtc@gmail.com>
2022-07-01 17:35:03 +0300

    100 % completed

--- src/index.js
+++ src/index.js
@@ -11,20 +11,20 @@ Notiflix.Notify.init({
   width: '400px',
   fontSize: '24px',
   info: {
-    background: '#7B68EE'
-  }
+    background: '#7B68EE',
+  },
 });
 
 const formEl = document.querySelector('.search-form');
 const galleryEl = document.querySelector('.gallery');
 galleryEl.addEventListener('click', onClickCardImgOpenFullImg);
 formEl.addEventListener('submit', onSearchQueryFormSubmit);
+let messageAfterLastImage = false;
 let amountFetchImages = null;
 let maxTottalImages = null;
 let currentTottalFetchImages = null;
 let page = 1;
 let searchQuery = '';
-
 
 async function onSearchQueryFormSubmit(event) {
   const inInput = event.currentTarget.elements.searchQuery;
@@ -34,9 +34,8 @@ async function onSearchQueryFormSubmit(event) {
     return;
   }
   searchQuery = inInput.value;
-  page = 1;
-  maxTottalImages = null;
-  currentTottalFetchImages = null;
+  resetConsts();
+
   try {
     const dataQuery = await fetchQuery(searchQuery, page);
     clearGallery();
@@ -45,6 +44,14 @@ async function onSearchQueryFormSubmit(event) {
     amountFetchImages = dataQuery.hits.length;
     calcPageAndImages();
     message(amountFetchImages);
+    lightbox.refresh();
+    if (amountFetchImages === 0) {
+      return;
+    } else {
+      setTimeout(() => {
+        messageAfterLastImage = true;
+      }, 100);
+    }
   } catch (error) {
     console.log(error);
   }
@@ -58,12 +65,21 @@ function message(amountFetchImages) {
   if (amountFetchImages > 0) {
     Notiflix.Notify.success(`Hooray! We found ${maxTottalImages} images.`);
   } else {
-    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
   }
 }
 
 function clearGallery() {
   galleryEl.innerHTML = '';
+}
+
+function resetConsts() {
+  page = 1;
+  maxTottalImages = null;
+  currentTottalFetchImages = null;
+  messageAfterLastImage = false;
 }
 
 function calcPageAndImages() {
@@ -76,29 +92,49 @@ function onClickCardImgOpenFullImg(e) {
 }
 
 const options = {
-  rootMargin: '300px',
+  rootMargin: '400px',
   threshold: 1.0,
 };
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(async entry => {
-    if (entry.isIntersecting && searchQuery !== ''
-      && maxTottalImages > currentTottalFetchImages) {
-
+    if (
+      entry.isIntersecting &&
+      searchQuery !== '' &&
+      maxTottalImages > currentTottalFetchImages
+    ) {
       try {
-  const dataQuery = await fetchQuery(searchQuery, page);
+        const dataQuery = await fetchQuery(searchQuery, page);
         markupImages(dataQuery.hits);
         calcPageAndImages();
-  } catch (error) {
-    console.log(error);
-  }
-
-    } else if (entry.isIntersecting && searchQuery !== ''
-      && maxTottalImages <= currentTottalFetchImages) {
-      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        lightbox.refresh();
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 }, options);
 
 observer.observe(document.querySelector('.observer-guard'));
 
+const optionsMessage = {
+  rootMargin: '1px',
+  threshold: 1.0,
+};
+
+const observerMessage = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (
+      entry.isIntersecting &&
+      maxTottalImages <= currentTottalFetchImages &&
+      messageAfterLastImage
+    ) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      messageAfterLastImage = false;
+    }
+  });
+}, optionsMessage);
+
+observerMessage.observe(document.querySelector('.observer-guard'));
--- src/marcupCard.js
+++ src/marcupCard.js
@@ -1,24 +1,22 @@
 
-
 export function markupImage({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-  return `<div class="photo-card">
- <a class="link-card" href="${largeImageURL}">
-      
+  return `<a class="link-card" href="${largeImageURL}">
+      <div class="photo-card">
       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>${likes}
+      <b>Likes</b><span>${likes}<span/>
     </p>
     <p class="info-item">
-      <b>Views</b>${views}
+      <b>Views</b><span>${views}<span/>
     </p>
     <p class="info-item">
-      <b>Comments</b>${comments}
+      <b>Comments</b><span>${comments}<span/>
     </p>
     <p class="info-item">
-      <b>Downloads</b>${downloads}
+      <b>Downloads</b><span>${downloads}<span/>
     </p>
-  </div></a>
-</div>`
+  </div>
+</div></a>`
 }
 
